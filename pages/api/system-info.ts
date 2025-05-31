21a9d80a0ea395feb109ac86483abca7390c30c5
Avyukt Soni <95626105+avyuktsoni0731@users.noreply.github.com>
2025-05-31 21:17:32 +0530

    refactoring pages

--- pages/api/system-info.ts
+++ pages/api/system-info.ts
@@ -1,21 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import si from 'systeminformation'
 
+function snapToStandardRAMSize(sizeGB: number): string {
+  const commonSizes = [4, 8, 16, 32, 64, 128];
+  const closest = commonSizes.reduce((prev, curr) =>
+    Math.abs(curr - sizeGB) < Math.abs(prev - sizeGB) ? curr : prev
+  );
+  return `${closest}`;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const [bios, cpu, mem, disk] = await Promise.all([
+    const [bios, cpu, mem, disk, graphics] = await Promise.all([
       si.bios(),
       si.cpu(),
       si.mem(),
       si.diskLayout(),
+      si.graphics(),
     ])
 
     res.status(200).json({
       biosVersion: `${bios.version} (${bios.releaseDate})`,
       cpu: `${cpu.manufacturer} ${cpu.brand}`,
-      memory: `${(mem.total / 1e9).toFixed(0)}GB`,
+      memory: `${snapToStandardRAMSize(mem.total / (1024 ** 3))}GB`,
       storage: `${(disk[0].size / 1e12).toFixed(1)}TB ${disk[0].type}`,
-      bootMode: 'UEFI'
+      bootMode: 'UEFI',
+      graphics: `${graphics.controllers[0].model}`,
     })
   } catch (err) {
     console.error(err)
